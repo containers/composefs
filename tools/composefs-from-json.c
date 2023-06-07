df1603c6a1a999901edf6ddac39a476b4bc1ef6f
Alexander Larsson <alexl@redhat.com>
2023-06-07 14:02:41 +0200

    composefs-from-json: Fix default format
    Signed-off-by: Alexander Larsson <alexl@redhat.com>

--- tools/composefs-from-json.c
+++ tools/composefs-from-json.c
@@ -627,7 +627,7 @@ static void do_file(struct lcfs_node_s *root, FILE *file)
 static void usage(const char *argv0)
 {
 	fprintf(stderr,
-		"usage: %s [--out=filedname] [--format=erofs|composefs] [--no-sandbox] jsonfile...\n",
+		"usage: %s [--out=filedname] [--format=erofs] [--no-sandbox] jsonfile...\n",
 		argv0);
 }
 
@@ -658,7 +658,7 @@ int main(int argc, char **argv)
 	};
 	struct lcfs_node_s *root;
 	struct lcfs_write_options_s options = { 0 };
-	const char *format = "composefs";
+	const char *format = "erofs";
 	ssize_t i;
 	int opt;
 	const char *out = NULL;
