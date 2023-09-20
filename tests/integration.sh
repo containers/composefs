a422a59812003404d6bac00119e75fa61178af32
Alexander Larsson <alexl@redhat.com>
2023-09-20 14:46:14 +0200

    tests: Add a new gendir based integration tests
    We previously only made an image of the host /usr/bin and verified it.
    This also adds a new image based on the gendir helper that generates
    a random image, which uses more file types.
    
    Signed-off-by: Alexander Larsson <alexl@redhat.com>

--- tests/integration.sh
+++ tests/integration.sh
@@ -10,30 +10,41 @@ rm ${cfsroot}/tmp -rf
 mkdir -p ${cfsroot}/{objects,roots,tmp}
 
 cd ${cfsroot}/tmp
-testsrc=/usr/bin
-mkcomposefs --print-digest --digest-store=${cfsroot}/objects ${testsrc} ${cfsroot}/roots/test.cfs | tee digest.txt
-prev_digest=$(cat digest.txt)
-new_digest=$(mkcomposefs --by-digest --print-digest-only ${testsrc})
-test "$prev_digest" = "$new_digest"
 
-if which fsck.erofs &>/dev/null; then
-    fsck.erofs ${cfsroot}/roots/test.cfs
-fi
+run_test() {
+    local testsrc=$1
+    local dumpdir_args="$2"
+    mkcomposefs --print-digest --digest-store=${cfsroot}/objects ${testsrc} ${cfsroot}/roots/test.cfs | tee digest.txt
+    prev_digest=$(cat digest.txt)
+    new_digest=$(mkcomposefs --by-digest --print-digest-only ${testsrc})
+    test "$prev_digest" = "$new_digest"
 
-mkdir -p mnt
-mount.composefs -o basedir=${cfsroot}/objects ${cfsroot}/roots/test.cfs mnt
-$orig/tests/dumpdir --no-nlink ${testsrc} > src-dump.txt
-$orig/tests/dumpdir --no-nlink mnt > mnt-dump.txt
-failed=
-if ! diff -u src-dump.txt mnt-dump.txt; then
-    failed=1
-fi
-if test -n "${failed}"; then
+    if which fsck.erofs &>/dev/null; then
+        fsck.erofs ${cfsroot}/roots/test.cfs
+    fi
+
+    mkdir -p mnt
+    mount.composefs -o basedir=${cfsroot}/objects ${cfsroot}/roots/test.cfs mnt
+    $orig/tests/dumpdir $dumpdir_args ${testsrc} > src-dump.txt
+    $orig/tests/dumpdir $dumpdir_args mnt > mnt-dump.txt
+    failed=
+    if ! diff -u src-dump.txt mnt-dump.txt; then
+        failed=1
+    fi
+    if test -n "${failed}"; then
+        umount mnt
+        exit 1
+    fi
+
+    new_digest=$(mkcomposefs --by-digest --print-digest-only mnt)
+    test "$prev_digest" = "$new_digest"
+
     umount mnt
-    exit 1
-fi
+}
 
-new_digest=$(mkcomposefs --by-digest --print-digest-only mnt)
-test "$prev_digest" = "$new_digest"
+run_test /usr/bin "--no-nlink"
 
-umount mnt
+# Don't create whiteouts, as they depend on a very recent kernel to work at all
+$orig/tests/gendir --privileged --nowhiteout ${cfsroot}/tmp/rootfs
+# nlink doesn't work for the toplevel dir in composefs, because that is from overlayfs, not erofs
+run_test ${cfsroot}/tmp/rootfs "--no-root-nlink"
