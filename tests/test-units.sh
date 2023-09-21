c5e216d824bc95d8b42d8168c5b0933be962554f
Alexander Larsson <alexl@redhat.com>
2023-09-21 15:25:08 +0200

    Test that the -o digest=XXX mount option work correctly
    Signed-off-by: Alexander Larsson <alexl@redhat.com>

--- tests/test-units.sh
+++ tests/test-units.sh
@@ -7,6 +7,8 @@ set -e
 workdir=$(mktemp -d /var/tmp/lcfs-test.XXXXXX)
 trap 'rm -rf -- "$workdir"' EXIT
 
+. test-lib.sh
+
 function makeimage () {
     local dir=$1
     ${VALGRIND_PREFIX} $BINDIR/mkcomposefs --digest-store=$dir/objects $dir/root $dir/test.cfs
@@ -44,12 +46,33 @@ function  test_objects () {
     fi
 }
 
-TESTS="test_inline test_objects"
+function  test_mount_digest () {
+    local dir=$1
+
+    if [ $has_fsverity = y ]; then
+        echo foo > $dir/root/a-file
+        makeimage $dir
+
+        $BINDIR/mount.composefs -o basedir=$dir/objects,digest=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa $dir/test.cfs $dir/mnt 2> $dir/stderr && fatal "non-fsverity mount should not succeed"
+        assert_file_has_content $dir/stderr "Image has no fs-verity"
+
+        fsverity enable $dir/test.cfs
+
+        $BINDIR/mount.composefs -o basedir=$dir/objects,digest=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa $dir/test.cfs $dir/mnt 2> $dir/stderr && fatal "wrong fsverity mount should not succeed"
+        assert_file_has_content $dir/stderr "Image has wrong fs-verity"
+
+        local DIGEST=$(fsverity measure $dir/test.cfs | awk "{ print \$1 }" | sed s/sha256://)
+
+        $BINDIR/mount.composefs -o basedir=$dir/objects,digest=$DIGEST $dir/test.cfs $dir/mnt 2> $dir/stderr || assert_file_has_content $dir/stderr "Permission denied"
+        umount $dir/mnt 2> $dir/stderr || true
+    fi
+}
+
+TESTS="test_inline test_objects test_mount_digest"
 res=0
 for i in $TESTS; do
     testdir=$(mktemp -d $workdir/$i.XXXXXX)
-    mkdir $testdir/root
-    mkdir $testdir/objects
+    mkdir $testdir/root $testdir/objects $testdir/mnt
     if $i $testdir ; then
         echo "Test $i: OK"
     else
