72cdee9248cf9a66dcb2ed3e98e20117c9a1a251
Alexander Larsson <alexl@redhat.com>
2023-10-10 13:29:09 +0200

    writer: Fix up error check for target links in tree
    We need to follow link chains when checking this, because
    none of the intermediate hardlinks are marked as in tree.
    
    Signed-off-by: Alexander Larsson <alexl@redhat.com>

--- libcomposefs/lcfs-writer.c
+++ libcomposefs/lcfs-writer.c
@@ -243,7 +243,8 @@ int lcfs_compute_tree(struct lcfs_ctx_s *ctx, struct lcfs_node_s *root)
 	for (node = root; node != NULL; node = node->next) {
 		for (size_t i = 0; i < node->children_size; i++) {
 			struct lcfs_node_s *child = node->children[i];
-			if (child->link_to != NULL && !child->link_to->in_tree) {
+			struct lcfs_node_s *link_to = follow_links(child);
+			if (child->link_to != NULL && !link_to->in_tree) {
 				/* Link to inode outside tree */
 				errno = EINVAL;
 				return -1;
