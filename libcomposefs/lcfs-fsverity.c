71ad1070843a2a5cff5762cfdd74d3958407078d
Jan Luebbe <jlu@pengutronix.de>
2024-05-12 20:19:11 +0200

    lcfs-fsverity: Add _GNU_SOURCE feature test define
    This file uses htole64(), so it should define _GNU_SOURCE.
    
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- libcomposefs/lcfs-fsverity.c
+++ libcomposefs/lcfs-fsverity.c
@@ -14,6 +14,8 @@
    You should have received a copy of the GNU Lesser General Public License
    along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
+#define _GNU_SOURCE
+
 #include "config.h"
 
 #include <sys/stat.h>
