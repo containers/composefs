0c8ce52075bd00b4b737a04389a8a29fc183ad0b
Colin Walters <walters@verbum.org>
2024-08-19 11:04:17 -0400

    test-should-fail: Fix distcheck
    We were writing to the sourcedir.
    
    Signed-off-by: Colin Walters <walters@verbum.org>

--- tests/test-should-fail.sh
+++ tests/test-should-fail.sh
@@ -7,7 +7,7 @@ shift
 tmpd=$(mktemp -d -t lcfs-test.XXXXXX)
 trap 'rm -rf -- "$tmpd"' EXIT
 for f in $@; do
-    if $bindir/mkcomposefs --from-file $f $tmpd/out.cfs >/dev/null 2>err.txt; then
+    if $bindir/mkcomposefs --from-file $f $tmpd/out.cfs &>/dev/null; then
         fatal "Test case $f should have failed"
     fi
     echo "ok $f"
