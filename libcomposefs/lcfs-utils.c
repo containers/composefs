93e9d17d73aa0d6dca1aa71bc8ccbfaccf7061ba
Colin Walters <walters@verbum.org>
2024-09-27 09:37:39 +0000

    utils: Move the other digest function to the impl file
    This function is a bit too big to be an inline, so
    move it to our new impl file where it's nicely next
    to its inverse.
    
    Signed-off-by: Colin Walters <walters@verbum.org>

--- libcomposefs/lcfs-utils.c
+++ libcomposefs/lcfs-utils.c
@@ -31,3 +31,30 @@ void digest_to_string(const uint8_t *csum, char *buf)
 	}
 	buf[j] = '\0';
 }
+
+int digest_to_raw(const char *digest, uint8_t *raw, int max_size)
+{
+	int size = 0;
+
+	while (*digest) {
+		char c1, c2;
+		int n1, n2;
+
+		if (size >= max_size)
+			return -1;
+
+		c1 = *digest++;
+		n1 = hexdigit(c1);
+		if (n1 < 0)
+			return -1;
+
+		c2 = *digest++;
+		n2 = hexdigit(c2);
+		if (n2 < 0)
+			return -1;
+
+		raw[size++] = (n1 & 0xf) << 4 | (n2 & 0xf);
+	}
+
+	return size;
+}
